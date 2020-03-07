e85f46fbcf9b27203c6e1da97ef01be6f0f3b211
devming <cmk330@gmail.com>
2020-03-07 16:54:03 +0900

    기본 UI 구현

---
+++ GeneralElection/util/SplashSegue.swift
@@ -0,0 +1,16 @@
+//
+//  SplashSegue.swift
+//  GeneralElection
+//
+//  Created by Minki on 2020/03/07.
+//  Copyright © 2020 Parsrich. All rights reserved.
+//
+
+import UIKit
+
+class SplashSegue: UIStoryboardSegue {
+    
+    override func perform() {
+        super.perform()
+    }
+}
