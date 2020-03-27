9c0567a39af4dd1dcbb21cf76d3d36e23e05bbeb
devming <cmk330@gmail.com>
2020-03-28 01:09:23 +0900

    앱버전 파이어베이스에 연동

--- GeneralElection/setting/viewmodel/SettingViewModel.swift
+++ GeneralElection/setting/viewmodel/SettingViewModel.swift
@@ -11,19 +11,27 @@ import NSObject_Rx
 
 class SettingViewModel: BaseViewModel {
     var settingList: [String]
+    var appVersion = "0.0.0"
+    var firebaseFetchSubject: PublishSubject<String>
     
     required init() {
         settingList = [String]()
+        firebaseFetchSubject = PublishSubject<String>()
         
         super.init()
+        setup()
+    }
+    
+    func setup() {
+        FirebaseManager.share.fetch { [weak self] isComplete in
+            guard let `self` = self else { return }
+            if !isComplete { return }
+            self.firebaseFetchSubject.onNext(FirebaseManager.share.stringValue(key: .appVersion))
+        }
         
-        settingList.append(contentsOf: ["파슬리치 소개",
-                                        "후원하기",
+        settingList.append(contentsOf: ["후원하기",
                                         "오류 문의",
-                                        "오픈소스 라이선스"])
-//        settingList.append(contentsOf: ["알림 설정",
-//                                        "파슬리치 소개",
-//                                        "후원하기",
-//                                        "오류 문의"])
+                                        "오픈소스 라이선스",
+                                        "앱 버전: \(appVersion)"])
     }
 }
