05b13cea5382d8bd277d73082458e44f0748ec19
Minki <chungminki@zuminternet.com>
2020-09-15 21:32:30 +0900

    Revert "개표결과 api 추가하려다가 잠시 중단"
    This reverts commit 995c3f5bb50db7a6bd07f1e075525d9d92ac1726.

--- GeneralElection/api/VoteResultApi.swift
+++ GeneralElection/api/VoteResultApi.swift
@@ -9,17 +9,17 @@
 import Foundation
 //sgId=20180613&sgTypecode=4&sggName=%EC%A2%85%EB%A1%9C%EA%B5%AC&sdName=%EC%84%9C%EC%9A%B8%ED%8A%B9%EB%B3%84%EC%8B%9C&wiwName=%EC%A2%85%EB%A1%9C%EA%B5%AC
 
-//class VoteResultApi: ApiBase<PartyPromiseBaseResponse> {
-//    
-//    init(partyName: String) {
-//        super.init()
-//        self.params = ["resultType": "json",
-//                       "serviceKey": ApiKeys.getKey,
-//                       "sgId": "20200415",
-//                       "partyName": partyName]
-//    }
-//    
-//    override var path: String {
-//        return "9760000/VoteXmntckInfoInqireService2/getXmntckSttusInfoInqire"
-//    }
-//}
+class PartyPromiseApi: ApiBase<PartyPromiseBaseResponse> {
+    
+    init(partyName: String) {
+        super.init()
+        self.params = ["resultType": "json",
+                       "serviceKey": ApiKeys.getKey,
+                       "sgId": "20200415",
+                       "partyName": partyName]
+    }
+    
+    override var path: String {
+        return "9760000/VoteXmntckInfoInqireService2/getXmntckSttusInfoInqire"
+    }
+}
